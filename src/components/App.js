64cb318f495a47139391feb525fc9f957671a511
sabir <sksabiruddin81@gmail.com>
2023-07-08 11:25:11 +0530

    submitting code-Sat Jul 08 2023 11:25:03 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const counter = useSelector((state) => state.counter);
+
+  const increment = () => {
+    dispatch({ type: "INCREMENT", payload: counter });
+  };
+
+  const decrement = () => {
+    dispatch({ type: "DECREMENT", payload: counter });
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>{counter}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
